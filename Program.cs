abddad594f2439f95b52cded11c95e4b22caabda
latifbahadir <latifbahadir97@gmail.com>
2020-08-13 10:36:52 +0300

    commit test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!a");
+            Console.WriteLine("Hello World!aaaaa");
         }
     }
 }
